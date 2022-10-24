a6f2928ae4be1f53d0a10d5971a1b1d6bde424c2
Dwight Banks <dbanks4271@gmail.com>
2022-10-23 19:13:54 -0500

    updated comments for code

--- getPrice.py
+++ getPrice.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
-#stored coinmarketcap api credentials in a separate file.. 
+
+
+
+#Libraries needed below... 
+#coinmarketcap api credentials stored in coincreds.py file called by import coincreds
+
 
 import pygsheets
 import json
 import coincreds
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+
+
+#function to get price of crypto current price... 
 def getPrice():	
 	#enter coin symbols.. ex. BTC
 	coins = ["XRP","HBAR","SAND","VET","SOLO","HNT","BTC","XLM","WLUNC","LRC","ETH","ANKR","GALA","SHIB","ADA","NU","COMP","GRT","FORTH","CVC","BUSD", "AVAX","BR34P","SAFUU","OHM","DRIP","DOGS","AFP","KAVA","CRO","LUNC","FLUX"]
 	cell = 3 
+	
+	#api call to loop through list of coin symbols current prices.... 
 	for i in coins:
 		url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
 		parameters = {
@@ -27,9 +37,13 @@ def getPrice():
 			response = session.get(url, params=parameters)
 			data = json.loads(response.text)
 			listing = (data['data'])
+			
+			#check against crypto symbol using coin name...
 			for j in listing[i]:
 				#enter coin name.. ex. Bitcoin
 				names = ["XRP", "Hedera", "The Sandbox", "VeChain", "Sologenic", "Helium",  "Bitcoin", "Stellar", "Wrapped LUNA Classic", "Loopring", "Ethereum", "Ankr", "Gala", "Shiba Inu", "Cardano", "NuCypher", "Compound", "The Graph", "Ampleforth Governance Token", "Civic", "Binance USD", "Avalanche", "BR34P", "Safuu", "Olympus v2", "Drip Network", "Dogs Token", "Animal Farm Pigs", "Kava", "Cronos", "Terra Classic", "Flux"]
+				
+				#update google sheet with current prices of crypto symbols.. 
 				for k in names:
 					if j['name'] == k:
 						price=(j['quote']['USD']['price'])
